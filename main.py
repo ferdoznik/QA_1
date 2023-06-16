eeb591724f087da9cf8e66013a316787cb67ffc4
ferdoznik <64578728+ferdoznik@users.noreply.github.com>
2023-06-16 21:13:14 +0300

    12313

--- main.py
+++ main.py
@@ -3,19 +3,22 @@ from selene.support.shared import browser
 from selene import be, have
 
 
-@pytest.fixture
+@pytest.fixture()
 def set_browser_resolution():
     browser.config.window_height = 1080
     browser.config.window_width = 1700
 
 
-def test_google_search(set_browser_resolution):
+@pytest.fixture()
+def open_url():
     browser.open('https://google.com')
+
+
+def test_google_search(set_browser_resolution, open_url):
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="rso"]').should(have.text('yashaka/selene: User-oriented Web UI browser tests in ...'))
 
 
-def test_random_input_search(set_browser_resolution):
-    browser.open('https://google.com')
+def test_random_input_search(set_browser_resolution, open_url):
     browser.element('[name="q"]').should(be.blank).type('askjdgqiuwehq').press_enter()
     browser.element('[id="result-stats"]').should(have.text('About 0 results'))
